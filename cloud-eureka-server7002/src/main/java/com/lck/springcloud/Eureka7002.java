5957900bfccd550511600762c1b6d50a441a61dd
lichangkui <lichangkui@chinadaas@com>
2020-12-16 23:33:43 +0800

    eureka集群搭建

--- cloud-eureka-server7002/src/main/java/com/lck/springcloud/Eureka7002.java
+++ cloud-eureka-server7002/src/main/java/com/lck/springcloud/Eureka7002.java
@@ -2,12 +2,12 @@ package com.lck.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class OrderMain80 {
+@EnableEurekaServer
+public class Eureka7002 {
     public static void main(String[] args) {
-        SpringApplication.run(OrderMain80.class,args);
+        SpringApplication.run(Eureka7002.class,args);
     }
 }
