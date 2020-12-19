f702c473c3c75eef3a702e845d96cb65df8658c2
lichangkui <lichangkui@chinadaas@com>
2020-12-19 15:52:44 +0800

    eureka集群搭建bug修复

--- cloud-eureka-server7002/src/main/java/com/lck/springcloud/EurekaMain7002.java
+++ cloud-eureka-server7002/src/main/java/com/lck/springcloud/EurekaMain7002.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class Eureka7002 {
+public class EurekaMain7002 {
     public static void main(String[] args) {
-        SpringApplication.run(Eureka7002.class,args);
+        SpringApplication.run(EurekaMain7002.class,args);
     }
 }
--- cloud-provider-payment8002/src/main/java/com/lck/springcloud/PaymentMain8002.java
+++ cloud-provider-payment8002/src/main/java/com/lck/springcloud/PaymentMain8002.java
@@ -1,4 +1,4 @@
-package springcloud;
+package com.lck.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
