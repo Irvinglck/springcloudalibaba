fe0399c24b7475c9361594dbff890723e2a4741c
lichangkui <lichangkui@chinadaas@com>
2021-01-11 22:02:11 +0800

    getwawy网管搭建

---
+++ cloud-getway-getway9527/src/main/java/com/lck/springcloud/CloudGetway9527.java
@@ -0,0 +1,14 @@
+package com.lck.springcloud;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class CloudGetway9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudGetway9527.class,args);
+    }
+}
---
+++ cloud-getway-getway9527/src/main/java/com/lck/springcloud/config/GetwayConfig.java
@@ -0,0 +1,17 @@
+package com.lck.springcloud.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GetwayConfig {
+    @Bean
+    public RouteLocator customerRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+        routes.route("path_route_path", r -> r.path("/guoji").uri("http://news.baidu.com/guoji")).build();
+        return routes.build();
+    }
+
+}
